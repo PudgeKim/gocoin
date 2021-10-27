aca7c50a9db9ea117a8f7170ae2ead15181327a1
PudgeKim <zxaq15@gmail.com>
2021-10-27 22:01:48 +0900

    add html to see blocks
    MVP CSS - 간단한 layout을 매우 쉽게 해줌

---
+++ explorer/explorer.go
@@ -0,0 +1,53 @@
+package explorer
+
+import (
+	"fmt"
+	"github.com/pudgekim/gocoin/blockchain"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+const (
+	port string = ":3030"
+	// main.go는 explorer 밖에 있으므로 explorer/ 를 붙여줘야함
+	templateDir string = "explorer/templates/"
+)
+
+var templates *template.Template
+
+type homeData struct {
+	// 대문자,소문자로 인한 public/private은 template에도 영향을 끼침
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	data := homeData{"Home", blockchain.GetBlockChain().AllBlocks()}
+	templates.ExecuteTemplate(w, "home", data)
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		templates.ExecuteTemplate(w, "add", nil)
+	case http.MethodPost:
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockChain().AddBlock(data)
+		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
+	}
+	templates.ExecuteTemplate(w, "add", nil)
+}
+
+func Start() {
+	// 여기서 pages 하위에 있는 것들을 glob함
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	// 위에서 만들어진 templates에 추가로 partials 하위 파일들을 glob
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+
+	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
+	fmt.Println("Listening on port:", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
