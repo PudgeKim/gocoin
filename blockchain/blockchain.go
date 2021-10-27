e1110875ab1f844b862c5382149765bfd7ede9e7
PudgeKim <zxaq15@gmail.com>
2021-10-27 19:46:42 +0900

    refactor code
    singleton pattern을 이용하여 refactor
    (sync.Once를 이용해 정말 1번만 실행되도록함)

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -3,47 +3,57 @@ package blockchain
 import (
 	"crypto/sha256"
 	"fmt"
+	"sync"
 )
 
 type block struct {
-	data     string
-	hash     string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockchain struct {
-	blocks []block
+	blocks []*block
 }
 
 // for singleton
 var b *blockchain
+var once sync.Once
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockChain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockChain().blocks[totalBlocks-1].Hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
 
 func GetBlockChain() *blockchain {
 	if b == nil {
-		// 위에서 var로 선언했기 때문에 :=가 아닌 =
-		b = &blockchain{}
+		once.Do(func() {
+			// 위에서 var로 선언했기 때문에 :=가 아닌 =
+			b = &blockchain{}
+			b.AddBlock("Genesis")
+		})
 	}
 	return b
 }
 
-func (b *blockchain) getLastHash() string {
-	if len(b.blocks) > 0 {
-		return b.blocks[len(b.blocks)-1].hash
-	}
-	return ""
-}
-
-func (b *blockchain) addBlock(data string) {
-	newBlock := block{data, "", b.getLastHash()}
-	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-	newBlock.hash = fmt.Sprintf("%x", hash)
-	b.blocks = append(b.blocks, newBlock)
-}
-
-func (b *blockchain) listBlocks() {
-	for _, block := range b.blocks {
-		fmt.Printf("Data: %s\n", block.data)
-		fmt.Printf("Hash: %s\n", block.hash)
-		fmt.Printf("Prev Hash: %s\n\n", block.prevHash)
-	}
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
 }
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/pudgekim/gocoin/blockchain"
+)
 
+func main() {
+	chain := blockchain.GetBlockChain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+
+	for _, block := range chain.AllBlocks() {
+		fmt.Println("Data: ", block.Data)
+	}
 }
